0a0c54ce330fb5aaa19ebb4d227931b63e9bae88
ChesonHuang <1348641216@qq.com>
2019-12-09 18:55:39 +0800

    init

---
+++ testcases/test_demo_1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*-coding:utf-8-*-
+import unittest
+import os
+import sys
+#import multiprocess
+
+class TestDemo1(unittest.TestCase):
+  def setUp(self):
+    '''
+    '''
+  def tearDown(self):
+    '''
+    '''
+
+  def test_demo_1():
+    '''
+    '''
+
+if __name__ == "__main__":
+  unittest.main()
